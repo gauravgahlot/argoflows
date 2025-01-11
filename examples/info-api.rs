f4003bc66fd3e63c95c6aeef8406ffc3b3b917af
Gaurav Gahlot <gauravgahlot0107@gmail.com>
2025-01-11 13:41:54 +0530

    update example
    Signed-off-by: Gaurav Gahlot <gauravgahlot0107@gmail.com>

--- examples/info-api.rs
+++ examples/info-api.rs
@@ -4,7 +4,12 @@ use argoflows::config::Config;
 fn main() {
     let token = std::env::var("ARGO_TOKEN").expect("the ARGO_TOKEN env variable must be set");
 
-    let cfg = Config::new(token);
+    let cfg = Config::builder()
+        .bearer_token(&token)
+        .danger_accept_invalid_certs(true)
+        .build();
+    let cfg = cfg.expect("failed to create client config");
+
     match info::get_version(&cfg) {
         Ok(v) => println!("{:?}\n", v),
         Err(e) => eprintln!("failed to get version: {:?}", e),
