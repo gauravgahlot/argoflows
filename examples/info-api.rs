a67cce45f88833b33e1ea3a80f6305b31bd2f017
Gaurav Gahlot <gauravgahlot0107@gmail.com>
2025-01-12 19:25:40 +0530

    api::info::collect_event - example
    Signed-off-by: Gaurav Gahlot <gauravgahlot0107@gmail.com>

--- examples/info-api.rs
+++ examples/info-api.rs
@@ -1,5 +1,6 @@
 use argoflows::api::info;
 use argoflows::config::Config;
+use argoflows::types::info::CollectEventRequest;
 
 fn main() {
     let token = std::env::var("ARGO_TOKEN").expect("the ARGO_TOKEN env variable must be set");
@@ -9,6 +10,20 @@ fn main() {
         .danger_accept_invalid_certs(true)
         .build();
     let cfg = cfg.expect("failed to create client config");
+
+    let payload = r#"{
+        "type": "custom-event",
+        "source": "ci-pipeline",
+        "data": {
+            "workflow": "event-driven-workflow",
+            "trigger": "build-success",
+            "message": "Build completed successfully!"
+        }
+    }"#;
+    match info::collect_event(&cfg, CollectEventRequest::new(payload)) {
+        Ok(r) => println!("{:?}\n", r),
+        Err(e) => eprintln!("failed to get info: {:?}", e),
+    }
 
     match info::get_info(&cfg) {
         Ok(r) => println!("{:?}\n", r),
