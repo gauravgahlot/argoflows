c498ebd82f814e7de2da7bc83886c44d89825a76
Gaurav Gahlot <gauravgahlot0107@gmail.com>
2025-01-12 19:25:59 +0530

    add types for info::collect_event
    Signed-off-by: Gaurav Gahlot <gauravgahlot0107@gmail.com>

--- src/error/info.rs
+++ src/error/info.rs
@@ -1,5 +1,13 @@
 use serde::{Deserialize, Serialize};
 
+/// struct for typed errors of method [`api::info::collect_event`]
+#[derive(Debug, Clone, Serialize, Deserialize)]
+#[serde(untagged)]
+pub enum CollectEventError {
+    DefaultResponse(super::GatewayRuntimeError),
+    UnknownValue(serde_json::Value),
+}
+
 /// Struct for typed errors of method [`api::info::get_info`]
 #[derive(Debug, Clone, Serialize, Deserialize)]
 #[serde(untagged)]
--- src/types/info.rs
+++ src/types/info.rs
@@ -1,6 +1,20 @@
 use serde::{Deserialize, Serialize};
 
 #[derive(Clone, Default, Debug, PartialEq, Serialize, Deserialize)]
+pub struct CollectEventRequest {
+    #[serde(rename = "name", skip_serializing_if = "Option::is_none")]
+    pub name: Option<String>,
+}
+
+impl CollectEventRequest {
+    pub fn new(name: &str) -> Self {
+        CollectEventRequest {
+            name: Some(name.to_owned()),
+        }
+    }
+}
+
+#[derive(Clone, Default, Debug, PartialEq, Serialize, Deserialize)]
 pub struct InfoResponse {
     #[serde(rename = "columns", skip_serializing_if = "Option::is_none")]
     pub columns: Option<Vec<Column>>,
