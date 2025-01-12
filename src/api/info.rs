c0abec8fc2aebbf27343565698966066803855a9
Gaurav Gahlot <gauravgahlot0107@gmail.com>
2025-01-12 18:55:14 +0530

    api::info::get_user_info
    Signed-off-by: Gaurav Gahlot <gauravgahlot0107@gmail.com>

--- src/api/info.rs
+++ src/api/info.rs
@@ -1,10 +1,10 @@
 use crate::config::Config;
 use crate::error::{
-    info::{GetInfoError, GetVersionError},
+    info::{GetInfoError, GetVersionError, UserInfoError},
     Error,
 };
 use crate::types::{
-    info::{InfoResponse, Version},
+    info::{InfoResponse, UserInfoResponse, Version},
     ResponseContent,
 };
 
@@ -25,6 +25,33 @@ pub fn get_info(config: &Config) -> Result<InfoResponse, Error<GetInfoError>> {
         serde_json::from_str(&content).map_err(Error::from)
     } else {
         let entity: Option<GetInfoError> = serde_json::from_str(&content).ok();
+        let err = ResponseContent {
+            status,
+            content,
+            entity,
+        };
+
+        Err(Error::Response(err))
+    }
+}
+
+pub fn get_user_info(config: &Config) -> Result<UserInfoResponse, Error<UserInfoError>> {
+    let uri = format!("{}/api/v1/userinfo", config.host);
+    let mut req_builder = config.client.request(reqwest::Method::GET, uri.as_str());
+
+    if let Some(bearer_token) = &config.bearer_token {
+        req_builder = req_builder.bearer_auth(bearer_token);
+    }
+
+    let req = req_builder.build()?;
+    let res = config.client.execute(req)?;
+    let status = res.status();
+    let content = res.text()?;
+
+    if !status.is_client_error() && !status.is_server_error() {
+        serde_json::from_str(&content).map_err(Error::from)
+    } else {
+        let entity: Option<UserInfoError> = serde_json::from_str(&content).ok();
         let err = ResponseContent {
             status,
             content,
