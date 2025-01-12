4d42ad10c257473466988570bac7cecb4e09e1f4
Gaurav Gahlot <gauravgahlot0107@gmail.com>
2025-01-12 19:26:22 +0530

    api::info::collect_event
    Signed-off-by: Gaurav Gahlot <gauravgahlot0107@gmail.com>

--- src/api/info.rs
+++ src/api/info.rs
@@ -1,12 +1,45 @@
+use serde_json;
+
 use crate::config::Config;
 use crate::error::{
-    info::{GetInfoError, GetVersionError, UserInfoError},
+    info::{CollectEventError, GetInfoError, GetVersionError, UserInfoError},
     Error,
 };
 use crate::types::{
-    info::{InfoResponse, UserInfoResponse, Version},
+    info::{CollectEventRequest, InfoResponse, UserInfoResponse, Version},
     ResponseContent,
 };
+
+pub fn collect_event(
+    config: &Config,
+    body: CollectEventRequest,
+) -> Result<serde_json::Value, Error<CollectEventError>> {
+    let uri = format!("{}/api/v1/tracking/event", config.host);
+    let mut req_builder = config.client.request(reqwest::Method::POST, uri.as_str());
+
+    if let Some(bearer_token) = &config.bearer_token {
+        req_builder = req_builder.bearer_auth(bearer_token);
+    }
+
+    req_builder = req_builder.json(&body);
+    let req = req_builder.build()?;
+    let res = config.client.execute(req)?;
+    let status = res.status();
+    let content = res.text()?;
+
+    if !status.is_client_error() && !status.is_server_error() {
+        serde_json::from_str(&content).map_err(Error::from)
+    } else {
+        let entity: Option<CollectEventError> = serde_json::from_str(&content).ok();
+        let err = ResponseContent {
+            status,
+            content,
+            entity,
+        };
+
+        Err(Error::Response(err))
+    }
+}
 
 pub fn get_info(config: &Config) -> Result<InfoResponse, Error<GetInfoError>> {
     let uri = format!("{}/api/v1/info", config.host);
