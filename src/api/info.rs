ab5c76afdb2dfba074307737c316175d6ddf0b57
Gaurav Gahlot <gauravgahlot0107@gmail.com>
2025-01-12 18:44:25 +0530

    api::info::get_info
    Signed-off-by: Gaurav Gahlot <gauravgahlot0107@gmail.com>

--- src/api/info.rs
+++ src/api/info.rs
@@ -1,6 +1,39 @@
 use crate::config::Config;
-use crate::error::{info::GetVersionError, Error};
-use crate::types::{info::Version, ResponseContent};
+use crate::error::{
+    info::{GetInfoError, GetVersionError},
+    Error,
+};
+use crate::types::{
+    info::{InfoResponse, Version},
+    ResponseContent,
+};
+
+pub fn get_info(config: &Config) -> Result<InfoResponse, Error<GetInfoError>> {
+    let uri = format!("{}/api/v1/info", config.host);
+    let mut req_builder = config.client.request(reqwest::Method::GET, uri.as_str());
+
+    if let Some(bearer_token) = &config.bearer_token {
+        req_builder = req_builder.bearer_auth(bearer_token);
+    }
+
+    let req = req_builder.build()?;
+    let res = config.client.execute(req)?;
+    let status = res.status();
+    let content = res.text()?;
+
+    if !status.is_client_error() && !status.is_server_error() {
+        serde_json::from_str(&content).map_err(Error::from)
+    } else {
+        let entity: Option<GetInfoError> = serde_json::from_str(&content).ok();
+        let err = ResponseContent {
+            status,
+            content,
+            entity,
+        };
+
+        Err(Error::Response(err))
+    }
+}
 
 pub fn get_version(config: &Config) -> Result<Version, Error<GetVersionError>> {
     let uri = format!("{}/api/v1/version", config.host);
