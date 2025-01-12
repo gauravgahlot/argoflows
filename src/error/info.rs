4e9490fb6a1109529d7dc4421684cd2bac19f67c
Gaurav Gahlot <gauravgahlot0107@gmail.com>
2025-01-12 18:42:13 +0530

    add types for info::get_info
    Signed-off-by: Gaurav Gahlot <gauravgahlot0107@gmail.com>

--- src/error/info.rs
+++ src/error/info.rs
@@ -1,5 +1,13 @@
 use serde::{Deserialize, Serialize};
 
+/// Struct for typed errors of method [`api::info::get_info`]
+#[derive(Debug, Clone, Serialize, Deserialize)]
+#[serde(untagged)]
+pub enum GetInfoError {
+    DefaultResponse(super::GatewayRuntimeError),
+    UnknownValue(serde_json::Value),
+}
+
 /// Struct for typed errors of method [`api::info::get_version`]
 #[derive(Debug, Clone, Serialize, Deserialize)]
 #[serde(untagged)]
--- src/types/info.rs
+++ src/types/info.rs
@@ -1,6 +1,20 @@
 use serde::{Deserialize, Serialize};
 
 #[derive(Clone, Default, Debug, PartialEq, Serialize, Deserialize)]
+pub struct InfoResponse {
+    #[serde(rename = "columns", skip_serializing_if = "Option::is_none")]
+    pub columns: Option<Vec<Column>>,
+    #[serde(rename = "links", skip_serializing_if = "Option::is_none")]
+    pub links: Option<Vec<Link>>,
+    #[serde(rename = "managedNamespace", skip_serializing_if = "Option::is_none")]
+    pub managed_namespace: Option<String>,
+    #[serde(rename = "modals", skip_serializing_if = "Option::is_none")]
+    pub modals: Option<std::collections::HashMap<String, bool>>,
+    #[serde(rename = "navColor", skip_serializing_if = "Option::is_none")]
+    pub nav_color: Option<String>,
+}
+
+#[derive(Clone, Default, Debug, PartialEq, Serialize, Deserialize)]
 pub struct Version {
     #[serde(rename = "buildDate")]
     pub build_date: String,
@@ -18,4 +32,35 @@ pub struct Version {
     pub platform: String,
     #[serde(rename = "version")]
     pub version: String,
+}
+
+/// A custom `Column` that will be exposed in the Workflow List View.
+#[derive(Clone, Default, Debug, PartialEq, Serialize, Deserialize)]
+pub struct Column {
+    /// The key of the label or annotation, e.g., `workflows.argoproj.io/completed`.
+    #[serde(rename = "key")]
+    pub key: String,
+    /// The name of this column, e.g., `Workflow Completed`.
+    #[serde(rename = "name")]
+    pub name: String,
+    /// The type of this column, `label` or `annotation`.
+    #[serde(rename = "type")]
+    pub r#type: String,
+}
+
+/// A `Link` to another app.
+#[derive(Clone, Default, Debug, PartialEq, Serialize, Deserialize)]
+pub struct Link {
+    /// The name of the link, E.g. `Workflow Logs` or `Pod Logs`
+    #[serde(rename = "name")]
+    pub name: String,
+    /// `workflow`, `pod`, `pod-logs`, `event-source-logs`, `sensor-logs`, `workflow-list` or `chat`
+    #[serde(rename = "scope")]
+    pub scope: String,
+    /// The `URL` can contain `${metadata.namespace}`, `${metadata.name}`,
+    /// `${status.startedAt}`, `${status.finishedAt}` or any other element
+    /// in workflow yaml, e.g.
+    /// `${io.argoproj.workflow.v1alpha1.metadata.annotations.userDefinedKey}`
+    #[serde(rename = "url")]
+    pub url: String,
 }
